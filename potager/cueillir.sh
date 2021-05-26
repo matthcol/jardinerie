7ebfada725e7bb776279e743b9c348a320ebcf5a
matthcol <matthias.colin@gmail.com>
2021-05-26 11:26:37 +0200

    improve cueillir again

--- potager/cueillir.sh
+++ potager/cueillir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Belle recolte chimique !"
 echo "pour la soupe aux choux rose"
-
+echo "très bon tout ça"
 echo "Savez vous ceuillir les choux transgÃ©nique ? oui/non"
 echo "Savez vous ceuillir les pommes bleus?"
